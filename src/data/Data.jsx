6f9db666554174e2504f3b9744bf8349fa401b93
Naomi García Robles <naomigarciarobles21@gmail.com>
2024-10-09 13:16:01 +0200

    Product Data

---
+++ src/data/Data.jsx
@@ -0,0 +1,22 @@
+const data = [
+  {
+    id: "1",
+    title: "Seo",
+    description: "Programació d'una web responsive completa",
+    price: 300,
+  },
+  {
+    id: "2",
+    title: "Ads",
+    description: "Programació d'una web responsive completa",
+    price: 400,
+  },
+  {
+    id: "3",
+    title: "Web",
+    description: "Programació d'una web responsive completa",
+    price: 500,
+  },
+];
+
+export default data;
