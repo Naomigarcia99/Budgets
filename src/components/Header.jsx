a9232dc58d47b420014f9a8fa7f8ae61584b4971
Naomi García Robles <naomigarciarobles21@gmail.com>
2024-10-14 10:59:32 +0200

    Header styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import headerImage from "../assets/header.png";
 
 export function Header() {
   return (
-    <div className="relative flex items-center justify-center">
-      <h1 className="absolute font-bold">Aconsegueix la millor qualitat</h1>
-      <img src={headerImage} alt="Header_image" />
+    <div className="container flex items-center justify-center mb-8">
+      <h1 className="text-5xl font-bold text-black">
+        Aconsegueix la millor qualitat
+      </h1>
     </div>
   );
 }
